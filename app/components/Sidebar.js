20e9cb4c302c527d02049f6a244dc6fae55307a2
Nick Hu <nick@nickhu.info>
2016-04-23 09:29:22 +0800

    fixed untracked files

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -15,6 +15,19 @@ function Sidebar (props){
       return (
         <div style={Styles.sidebar}><h2>{obj.name}</h2>
         <ul style={Styles.photoTable} id="imageList">{photos}</ul>
+        <div class="mdl-card mdl-shadow--4dp">
+        <div class="mdl-card__media"><img src="skytower.jpg" width="173" height="157" border="0"
+         alt="" style="padding:10px;">
+        </div>
+        <div class="mdl-card__supporting-text">
+          Auckland Sky Tower, taken March 24th, 2014
+        </div>
+        <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland,
+        New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure
+        in the Southern Hemisphere.
+        </div>
+      </div>
         </div>
       )
 
